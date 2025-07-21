252bd0bacba4321c2b27418b04f90d7a1fe2126b
Ebnou Abdem Seyid Abdellahi <65211672+Seyid2020@users.noreply.github.com>
2025-07-21 14:00:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 st.markdown("""
 ### 📝 Remplir le formulaire détaillé
-[👉 Cliquez ici pour accéder au formulaire Microsoft Forms](
-https://forms.office.com/Pages/ResponsePage.aspx?id=V2FiOUegiUaHom-mRctct4nQ0_9pFOVOtOpqm9QvhpxUNDlWTVk1UjI4VldPS0xWUk1EVUZaMEs4Ty4u)
+[👉 Cliquez ici pour accéder au formulaire Microsoft Forms]
+(https://forms.office.com/Pages/ResponsePage.aspx?id=V2FiOUegiUaHom-mRctct4nQ0_9pFOVOtOpqm9QvhpxUNDlWTVk1UjI4VldPS0xWUk1EVUZaMEs4Ty4u)
 """)
 
 st.info("""
